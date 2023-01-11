892c1b946f750bc5094a68736b1e14e244c3d2e2
KungFuTurtle <m.rfischer5235@gmail.com>
2023-01-10 21:47:44 -0600

    final push tonight

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-// router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
 //     // Add a comment describing the purpose of the render method
 //     // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-//     res.render('all');
-//   });
+    res.render('all');
+  });
 
 // //const userRoutes = require('./user-routes.js');
 //const postRoutes = require('./post-routes');
