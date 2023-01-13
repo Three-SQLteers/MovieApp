bfe17d2950478b66415d6d2f11666c39ee2aab84
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-01-12 20:00:37 -0600

    worked on login, logout ,signup js and handlebars

---
+++ public/js/signup.js
@@ -0,0 +1,32 @@
+async function signupFormHandler(event){
+    event.preventDefault();
+
+    //this code will get data from the form
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if(username && password){
+        const response = await fetch('/api/users',{
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers:{'Content-Type': 'application/json'}
+        });
+
+        // checking the response status
+        if(response.ok){
+            console.log('Account created!');
+
+            // this is login handler
+            document.location.replace('/login');
+        }else {
+            alert(response.statusText);
+        }
+    }
+}
+
+
+document.querySelector('#signup-form').addEventListener('submit',signupFormHandler);
+
