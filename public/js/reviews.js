309aef60d7e98dd1da797fb62299b36c845a3479
KungFuTurtle <m.rfischer5235@gmail.com>
2023-01-13 19:17:41 -0600

    comment post

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,8 +1,36 @@
-const movieReview = document.querySelector("#movieComments");
+
 const reviewBtn = document.querySelector("#reviewBtn");
 
 
 
-reviewBtn.addEventListener("click",()=>{
-    
+async function signupFormHandler(event){
+    event.preventDefault();
+
+    //this code will get data from the form
+    const movieReview = document.querySelector("#movieComments");
+
+
+    if(movieReview && password){
+        const response = await fetch('/api/users',{
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers:{'Content-Type': 'application/json'}
+        });
+
+        // checking the response status
+        if(response.ok){
+            console.log('Account created!');
+
+            // this is login handler
+            document.location.replace('/login');
+        }else {
+            alert(response.statusText);
+        }
+    }
 }
+
+
+document.querySelector('#signup-form').addEventListener('submit',signupFormHandler);
