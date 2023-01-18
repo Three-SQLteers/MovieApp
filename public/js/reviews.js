31b928be435ebce663be50bad33e9f2e07d66871
KungFuTurtle <m.rfischer5235@gmail.com>
2023-01-17 21:48:57 -0600

    push

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,81 +1,81 @@
-const movieInfo = document.getElementById("movie-info");
-const searchResults = document.getElementById("search-results");
+// const movieInfo = document.getElementById("movie-info");
+// const searchResults = document.getElementById("search-results");
 
 
-const API_KEY = 'api_key=c37d08875afe5ad2df252dfaa348f06b';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+// const API_KEY = 'api_key=c37d08875afe5ad2df252dfaa348f06b';
+// const BASE_URL = 'https://api.themoviedb.org/3';
+// const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 
-function showMovies(data) {
+// function showMovies(data) {
 
 
-//     data.results.forEach(movie => {
-//         const { title, poster_path, genre, release_date, id } = movie;
+// //     data.results.forEach(movie => {
+// //         const { title, poster_path, genre, release_date, id } = movie;
        
-        let movieCard = document.createElement("div");
-        let movieTitle = document.createElement("h2");
-        let poster = document.createElement("img");
+//         let movieCard = document.createElement("div");
+//         let movieTitle = document.createElement("h2");
+//         let poster = document.createElement("img");
 
-//         let listEl = document.createElement("ul");
+// //         let listEl = document.createElement("ul");
 
-        let rating = document.createElement("li");
-        let Movierelease_date = document.createElement("p");
-        let movieId = document.createElement("p");
-        let movieGenre = document.createElement("p");
-        let anchorContainer = document.createElement("a");
-        let selectMovie = document.createElement("button");
+//         let rating = document.createElement("li");
+//         let Movierelease_date = document.createElement("p");
+//         let movieId = document.createElement("p");
+//         let movieGenre = document.createElement("p");
+//         let anchorContainer = document.createElement("a");
+//         let selectMovie = document.createElement("button");
 
-        movieCard.setAttribute("class","movieCard")
-        searchResults.setAttribute("class", "searchResults")
-        selectMovie.setAttribute("id","reviewbtn" ) 
-        anchorContainer.setAttribute('href', `/moviereviews/${id}`)
-        movieId.setAttribute("id", "fetchMovieId")        
+//         movieCard.setAttribute("class","movieCard")
+//         searchResults.setAttribute("class", "searchResults")
+//         selectMovie.setAttribute("id","reviewbtn" ) 
+//         anchorContainer.setAttribute('href', `/moviereviews/${id}`)
+//         movieId.setAttribute("id", "fetchMovieId")        
         
-        movieTitle.textContent = title;
-        Movierelease_date.textContent = release_date;
-        movieId.textContent= id;
-        movieGenre.textContent = genre;
-        selectMovie.textContent = "Review Movie";
+//         movieTitle.textContent = title;
+//         Movierelease_date.textContent = release_date;
+//         movieId.textContent= id;
+//         movieGenre.textContent = genre;
+//         selectMovie.textContent = "Review Movie";
 
-//         poster.setAttribute('src', `https://image.tmdb.org/t/p/w200${poster_path}`)
-//         poster.setAttribute('alt', `${title}'s movie poster`)
-
-
-        anchorContainer.append(selectMovie)
-        movieCard.append(poster, movieId, movieTitle, movieGenre, Movierelease_date, anchorContainer)
+// //         poster.setAttribute('src', `https://image.tmdb.org/t/p/w200${poster_path}`)
+// //         poster.setAttribute('alt', `${title}'s movie poster`)
 
 
-        searchResults.appendChild(movieCard);
-
-//     })
-// };
-
-// async function reviewFormHandler(event){
-//     event.preventDefault();
-
-//     //this code will get data from the form
-//     const review_string = document.querySelector("#movieComments").value.trim();
+//         anchorContainer.append(selectMovie)
+//         movieCard.append(poster, movieId, movieTitle, movieGenre, Movierelease_date, anchorContainer)
 
 
-//     if(review_string){
-//         const response = await fetch(`/api/review`,{
-//             method: 'POST',
-//             body: JSON.stringify({review_string}),
-//             headers:{'Content-Type': 'application/json'}
+//         searchResults.appendChild(movieCard);
+
+// //     })
+// // };
+
+async function reviewFormHandler(event){
+    event.preventDefault();
+
+    //this code will get data from the form
+    const review_string = document.querySelector("#movieComments").value.trim();
+
+
+    if(review_string){
+        const response = await fetch(`/api/review`,{
+            method: 'POST',
+            body: JSON.stringify({review_string}),
+            headers:{'Content-Type': 'application/json'}
            
-//         });
-//  console.log('Post created!', review_string);
-//         // checking the response status
-//         if(response.ok){
-//             console.log('Post created!');
+        });
+ console.log('Post created!', review_string);
+        // checking the response status
+        if(response.ok){
+            console.log('Post created!');
 
-//             // this is login handler
+            // this is login handler
             
-//         }else {
-//             alert(response.statusText);
-//         }
-//     }
-// }
+        }else {
+            alert(response.statusText);
+        }
+    }
+}
 
 // document.querySelector('#reviewBtn').addEventListener('click',reviewFormHandler);
 
