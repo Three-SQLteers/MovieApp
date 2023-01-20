52fb6657e642a8e1c664c0f550c8eb3b1f63c853
Mark Calcagno <mcalcagn@gmail.com>
2023-01-19 18:39:58 -0600

    got slider integers to post to database

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,19 +1,18 @@
-
-
-
-
-
 async function reviewFormHandler(event){
     event.preventDefault();
 
     //this code will get data from the form
     const review_string = document.querySelector("#movieComments").value.trim();
-const movie_id = window.location.href.split("/")[4]
+    const quality_review_integer = document.querySelector('#quality_review_integer').value; 
+    const fun_review_integer = document.querySelector('#fun_review_integer').value; 
+    const movie_id = window.location.href.split("/")[4]
 
     if(review_string){
         const response = await fetch(`/api/review`,{
             method: 'POST',
             body: JSON.stringify({
+                quality_review_integer: quality_review_integer,
+                fun_review_integer: fun_review_integer,
                 review_string: review_string, 
                 movie_id: movie_id,
         }),
